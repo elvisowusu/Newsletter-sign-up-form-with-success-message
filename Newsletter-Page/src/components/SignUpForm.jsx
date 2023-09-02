8604af8e36174b00848b2b1c6bd8c8ac11de094e
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-02 17:37:43 +0000

    changed the h2 to an h1 element

--- Newsletter-Page/src/components/SignUpForm.jsx
+++ Newsletter-Page/src/components/SignUpForm.jsx
@@ -7,9 +7,11 @@ export const SignUpForm =()=>{
     return(
         <div className="mainPage">
             <div className="signUp">
-                <h2>Stay updated!</h2>
+                <h1>Stay updated!</h1>
                 <p>Join 60,000+ product managers receiving monthly updates on:</p>
-                <div className=""></div>
+                <div className="updateList">
+
+                </div>
             </div>
             <img src={image} alt="SignUp image here" />
         </div>
