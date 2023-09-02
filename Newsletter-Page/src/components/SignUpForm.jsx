38fd99c6ee938532d5687dcb6bd3110bb790b072
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-02 17:44:05 +0000

    edited the position of the form

--- Newsletter-Page/src/components/SignUpForm.jsx
+++ Newsletter-Page/src/components/SignUpForm.jsx
@@ -10,16 +10,14 @@ export const SignUpForm =()=>{
                 <h1>Stay updated!</h1>
                 <p>Join 60,000+ product managers receiving monthly updates on:</p>
                 <div className="updateList">
-                    <ul>
                         <li>Product discovery and building what matters</li>
                         <li>Measuring to ensure updates are a success</li>
                         <li>Add much more</li>
-                    </ul>
+                </div>
                     <form action="">
                         <label htmlFor="">Email Address</label>
                         <input type="email" />
                     </form>
-                </div>
             </div>
             <img src={image} alt="SignUp image here" />
         </div>
