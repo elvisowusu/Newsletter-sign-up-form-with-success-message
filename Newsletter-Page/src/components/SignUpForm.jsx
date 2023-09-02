352b1ae3698733cbecbba87b0f2d525f2ad39bba
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-02 17:36:43 +0000

    added an h1,paragraph and an updateList div

--- Newsletter-Page/src/components/SignUpForm.jsx
+++ Newsletter-Page/src/components/SignUpForm.jsx
@@ -7,7 +7,9 @@ export const SignUpForm =()=>{
     return(
         <div className="mainPage">
             <div className="signUp">
-
+                <h2>Stay updated!</h2>
+                <p>Join 60,000+ product managers receiving monthly updates on:</p>
+                <div className=""></div>
             </div>
             <img src={image} alt="SignUp image here" />
         </div>
