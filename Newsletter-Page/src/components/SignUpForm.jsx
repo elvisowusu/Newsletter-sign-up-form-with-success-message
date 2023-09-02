18944065409e5a31339ad142d3596d500c3afb06
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-02 17:41:05 +0000

    added content to the list

--- Newsletter-Page/src/components/SignUpForm.jsx
+++ Newsletter-Page/src/components/SignUpForm.jsx
@@ -10,7 +10,11 @@ export const SignUpForm =()=>{
                 <h1>Stay updated!</h1>
                 <p>Join 60,000+ product managers receiving monthly updates on:</p>
                 <div className="updateList">
-
+                    <ul>
+                        <li>Product discovery and building what matters</li>
+                        <li>Measuring to ensure updates are a success</li>
+                        <li>Add much more</li>
+                    </ul>
                 </div>
             </div>
             <img src={image} alt="SignUp image here" />
