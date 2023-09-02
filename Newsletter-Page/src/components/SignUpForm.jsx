44e8f347c629357c1361e3e577689f6ed9b854ca
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-02 17:30:38 +0000

    imported the image to SignUpForm.jsx

--- Newsletter-Page/src/components/SignUpForm.jsx
+++ Newsletter-Page/src/components/SignUpForm.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import '../styles/SignUpForm.css'
+import image from '../assets/signUpImage.svg'
 
 
 export const SignUpForm =()=>{
     return(
         <div className="mainPage">
-            
+            <img src={image} alt="SignUp image here" />
         </div>
     )
 }
